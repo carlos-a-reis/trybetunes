aee80c7fff901c7f95f57f0be2ead639e8d50919
Carlos <hadqen@gmail.com>
2023-05-17 18:51:29 -0300

    feat: adiciona a página de 'page not found' com estilo responsivo

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import error from '../images/error.svg';
+import '../CSS/notFound.css';
 
 class NotFound extends React.Component {
   render() {
     return (
-      <div>Página não encontrada</div>
+      <div className="not-found-page">
+        <img src={ error } alt="icone de erro" />
+        <div className="error-message">
+          <p>Página</p>
+          <p>Não</p>
+          <p>Encontrada</p>
+        </div>
+      </div>
     );
   }
 }
